2d2b0648042733a5e56c68cb2f4965f027bc982f
Jon Birdsall <cuedain@gmail.com>
2016-09-10 19:32:33 -0700

    elsif with elsif

--- age.pl
+++ age.pl
@@ -6,16 +6,12 @@ print "What is your age? ";
 my $age = <STDIN>;
 if ($age < 5) {
 	print "You're too young for school.";
+} elsif ($age < 18) {
+	print "You must go to school!";
+} elsif ($age < 21) {
+	print "In most countries you can vote.";
 } else {
-	if ($age < 18) {
-		print "You must go to school!";
-	} else {
-		if ($age < 21) {
-			print "In most countries you can vote.";
-		} else {
-			print "You can drink booze in the USA!";
-		}
-	}
+	print "You can drink booze in the USA!";
 }
 
 
