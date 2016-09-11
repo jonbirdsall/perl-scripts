29dcc3eec4644132f85603ba9d981e9c4f851ee0
Jon Birdsall <cuedain@gmail.com>
2016-09-10 19:23:16 -0700

    Nested if statements

--- age.pl
+++ age.pl
@@ -6,8 +6,14 @@ print "What is your age?";
 my $age = <STDIN>;
 if ($age >= 18) {
 	print "You're old enough to vote in most countries!";
+	if ($age >= 21) {
+		print "\nYou can drink booze in the USA, too!";
+	}
 } else {
 	print "No voting for you.";
+	if ($age < 12) {
+		print "\nBut, you're in grade school so you don't care!";
+	}
 }
 
 
