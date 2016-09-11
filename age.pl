5e3d70b3cd418dae8ac21e9eb2edb9aa01e97b2f
Jon Birdsall <cuedain@gmail.com>
2016-09-10 19:29:55 -0700

    elsif without the elsif

--- age.pl
+++ age.pl
@@ -2,17 +2,19 @@
 use strict;
 use warnings;
 
-print "What is your age?";
+print "What is your age? ";
 my $age = <STDIN>;
-if ($age >= 18) {
-	print "You're old enough to vote in most countries!";
-	if ($age >= 21) {
-		print "\nYou can drink booze in the USA, too!";
-	}
+if ($age < 5) {
+	print "You're too young for school.";
 } else {
-	print "No voting for you.";
-	if ($age < 12) {
-		print "\nBut, you're in grade school so you don't care!";
+	if ($age < 18) {
+		print "You must go to school!";
+	} else {
+		if ($age < 21) {
+			print "In most countries you can vote.";
+		} else {
+			print "You can drink booze in the USA!";
+		}
 	}
 }
 
